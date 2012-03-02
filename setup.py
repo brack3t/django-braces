d79f02c9cc6642bce139ecbd5b8ebfc62cff1075
Kenneth Love <kenneth@gigantuan.net>
2012-03-02 12:24:23 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="braces",
+    version="0.1.0",
+    description="Reusable, generic mixins for Django",
+    long_description="Mixins to add easy functionality to Django class-based views, forms, and models.",
+    keywords="django, views, forms, mixins",
+    author="Kenneth Love <kenneth@brack3t.com>, Chris Jones <chris@brack3t.com>",
+    author_email="kenneth@brack3t.com",
+    url="https://github.com/brack3t/django-braces/",
+    license="BSD",
+    packages=["braces"],
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+)
