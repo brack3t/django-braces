91e01752cbbf4dfe784ec4b9377d8f27adea2b3a
Chris Jones <chris@brack3t.com>
2012-05-30 15:13:47 -0700

    Added a new SelectRelatedMixin.

--- braces/views.py
+++ braces/views.py
@@ -154,3 +154,26 @@ class SetHeadlineMixin(object):
                 u"%(cls)s.get_headline()." % {"cls": self.__class__.__name__
             })
         return self.headline
+
+
+class SelectRelatedMixin(object):
+    """
+    Mixin allows you to provide a tuple or list of related models to
+    perform a select_related on.
+    """
+    select_related = None
+
+    def get_queryset(self):
+        if self.select_related is None:
+            raise ImproperlyConfigured(u"%(cls)s is missing the select_related "
+                "property. This must be a tuple or list." % {
+                    "cls": self.__class__.__name__})
+
+        if not isinstance(self.select_related, (tuple, list)):
+            raise ImproperlyConfigured(u"%(cls)s's select_related property "
+                "must be a tuple or list." % {"cls": self.__class__.__name__})
+
+        queryset = super(SelectRelatedMixin, self).get_queryset()
+        return queryset.select_related(
+            ", ".join(self.select_related)
+        )
