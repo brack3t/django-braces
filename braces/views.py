b2e15d318447b1a65687c2e3d81d6e29162d82bf
Chris Jones <chris@brack3t.com>
2012-08-31 17:35:58 -0700

    Docs, split checks out into their own methods to make dispatch a little easier to read, fixed bad usage of isinstance.

--- braces/views.py
+++ braces/views.py
@@ -101,6 +101,40 @@ class PermissionRequiredMixin(object):
 
 class MultiplePermissionsRequiredMixin(object):
     """
+    View mixin which allows you to specify two types of permission
+    requirements. The `permissions` attribute must be a dict which
+    specifies two keys, `all` and `any`. You can use either one on
+    it's own or combine them. Both keys values are required be a list or
+    tuple of permissions in the format of
+    <app label>.<permission codename>
+
+    By specifying the `all` key, the user must have all of
+    the permissions in the passed in list.
+
+    By specifying The `any` key , the user must have ONE of the set
+    permissions in the list.
+
+    Class Settings
+        `permissions` - This is required to be a dict with one or both
+            keys of `all` and/or `any` containing a list or tuple of
+            permissions in the format of <app label>.<permission codename>
+        `login_url` - the login url of site
+        `redirect_field_name` - defaults to "next"
+        `raise_exception` - defaults to False - raise 403 if set to True
+
+    Example Usage
+        class SomeView(MultiplePermissionsRequiredMixin, ListView):
+            ...
+            #required
+            permissions = {
+                "all": (blog.add_post, blog.change_post),
+                "any": (blog.delete_post, user.change_user)
+            }
+
+            #optional
+            login_url = "/signup/"
+            redirect_field_name = "hollaback"
+            raise_exception = True
     """
     login_url = settings.LOGIN_URL # LOGIN_URL from project settings
     permissions = None # Default required perms to none
@@ -108,26 +142,16 @@ class MultiplePermissionsRequiredMixin(object):
     redirect_field_name = REDIRECT_FIELD_NAME #Set by django.contrib.auth
 
     def dispatch(self, request, *args, **kwargs):
-        if self.permissions is None or self.permissions is not isinstance(dict):
-            raise ImproperlyConfigured("'PermissionsRequiredMixin' requires"
-                "'permissions' attribute to be set to a dict.")
+        self._check_permissions_attr()
 
         perms_all = self.permissions.get('all') or None
         perms_any = self.permissions.get('any') or None
 
-        if perms_all is None and perms_any is None:
-            raise ImproperlyConfigured("'PermissionsRequiredMixin' requires"
-                "'permissions' attribute to be set to a dict and the 'any'"
-                "or 'all' key to be set.")
+        self._check_permissions_keys_set(perms_all, perms_any)
+        self._check_perms_keys("all", perms_all)
+        self._check_perms_keys("any", perms_any)
 
-        if perms_all and perms_all is not isinstance(list, tuple):
-            raise ImproperlyConfigured("'PermissionsRequiredMixin' requires"
-                "permissions dict 'all' value to be a list or tuple.")
-
-        if perms_any and perms_any is not isinstance(list, tuple):
-            raise ImproperlyConfigured("'PermissionsRequiredMixin' requires"
-                "permissions dict 'any' value to be a list or tuple.")
-
+        # If perms_all, check that user has all permissions in the list/tuple
         if perms_all:
             if not request.user.has_perms(perms_all):
                 if self.raise_exception:
@@ -136,6 +160,7 @@ class MultiplePermissionsRequiredMixin(object):
                 tup = self.login_url, self.redirect_field_name, path
                 return HttpResponseRedirect("%s?%s=%s" % tup)
 
+        # If perms_any, check that user has at least one in the list/tuple
         if perms_any:
             has_one_perm = False
             for perm in perms_any:
@@ -152,6 +177,35 @@ class MultiplePermissionsRequiredMixin(object):
 
         return super(MultiplePermissionsRequiredMixin, self).dispatch(request,
             *args, **kwargs)
+
+    def _check_permissions_attr(self):
+        """
+        Check permissions attribute is set and that it is a dict.
+        """
+        if self.permissions is None or not isinstance(self.permissions, dict):
+            raise ImproperlyConfigured("'PermissionsRequiredMixin' requires "
+                "'permissions' attribute to be set to a dict.")
+
+    def _check_permissions_keys_set(self, perms_all=None, perms_any=None):
+        """
+        Check to make sure the keys `any` or `all` are not both blank.
+        If both are blank either an empty dict came in or the wrong keys
+        came in. Both are invalid and should raise an exception.
+        """
+        if perms_all is None and perms_any is None:
+            raise ImproperlyConfigured("'PermissionsRequiredMixin' requires"
+                "'permissions' attribute to be set to a dict and the 'any' "
+                "or 'all' key to be set.")
+
+    def _check_perms_keys(self, key=None, perms=None):
+        """
+        If the permissions list/tuple passed in is set, check to make
+        sure that it is of the type list or tuple.
+        """
+        if perms and not isinstance(perms, (list, tuple)):
+            raise ImproperlyConfigured("'MultiplePermissionsRequiredMixin' "
+                "requires permissions dict '%s' value to be a list "
+                "or tuple." % key)
 
 
 class UserFormKwargsMixin(object):
