6ad0167db795617592dffb34e185637ab537ba3f
Kenneth Love <kenneth@gigantuan.net>
2012-09-13 15:44:29 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-braces",
-    version="0.1.5",
+    version="0.1.6",
     description="Reusable, generic mixins for Django",
     long_description="Mixins to add easy functionality to Django class-based views, forms, and models.",
     keywords="django, views, forms, mixins",
