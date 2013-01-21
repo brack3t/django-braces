74ee64ac1db41c157318a8d34cd0c6cd82a8565f
Chris Jones <chris@brack3t.com>
2013-01-21 10:14:42 -0800

    bump version number and add to change log.

--- README.rst
+++ README.rst
@@ -20,3 +20,8 @@ CHANGE LOG
   datetimes)
 * Fixed all mixins that have `raise_exception` as an argument to properly raise a
   `PermissionDenied` exception to allow for custom 403s.
+
+1.2.2
+-----
+
+* Try importing the built-in json module first, drop back to Django if necessary. Django 1.5 compatibility.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-braces",
-    version="0.2.1",
+    version="0.2.2",
     description="Reusable, generic mixins for Django",
     long_description="Mixins to add easy functionality to Django class-based views, forms, and models.",
     keywords="django, views, forms, mixins",
