37b31bc9a7868cb038968c626636a487a901c655
Chris Jones <chris@brack3t.com>
2013-02-27 13:44:45 -0800

    Rewrite of the login required mixin so that it has the same features as the other 'access' mixins.

--- braces/views.py
+++ braces/views.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import redirect_to_login
 from django.core import serializers
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
@@ -41,16 +40,25 @@ class CreateAndRedirectToEditView(CreateView):
 
 class LoginRequiredMixin(object):
     """
-    View mixin which verifies that the user has authenticated.
+    View mixin which verifies that the user is authenticated.
 
     NOTE:
         This should be the left-most mixin of a view.
     """
+    login_url = settings.LOGIN_URL  # LOGIN_URL from project settings
+    raise_exception = False  # Default whether to raise an exception to none
+    redirect_field_name = REDIRECT_FIELD_NAME  # Set by django.contrib.auth
 
-    @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        return super(LoginRequiredMixin, self).dispatch(request,
-            *args, **kwargs)
+        if not request.user.is_authenticated():
+            if self.raise_exception:
+                raise PermissionDenied  # return a forbidden response
+            else:
+                return redirect_to_login(request.get_full_path(),
+                    self.login_url, self.redirect_field_name)
+
+        return super(LoginRequiredMixin, self).dispatch(request, *args,
+            **kwargs)
 
 
 class CsrfExemptMixin(object):
