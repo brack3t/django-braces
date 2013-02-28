96b1173037ccd0c87bfef89017c6e5001e54b92b
Chris Jones <chris@brack3t.com>
2013-02-28 11:37:46 -0800

    New AccessMixin which provides get_login_url and get_redirect_field_name methods to all access mixins. Not fully implemented, tested or implemented yet.

--- braces/views.py
+++ braces/views.py
@@ -46,24 +46,49 @@ class CreateAndRedirectToEditView(CreateView):
             "No URL to reverse. Provide a success_url_name.")
 
 
-class LoginRequiredMixin(object):
+class AccessMixin(object):
+    """
+    """
+    login_url = settings.LOGIN_URL  # LOGIN_URL from project settings
+    raise_exception = False  # Default whether to raise an exception to none
+    redirect_field_name = REDIRECT_FIELD_NAME  # Set by django.contrib.auth
+
+    def get_login_url(self):
+        """
+        """
+        if self.login_url is None:
+            raise ImproperlyConfigured("%(cls)s is missing the login_url. "
+                "Define %(cls)s.login_url or override "
+                "%(cls)s.get_login_url()." % {"cls": self.__class__.__name__})
+
+        return self.login_url
+
+    def get_redirect_field_name(self):
+        """
+        """
+        if self.redirect_field_name is None:
+            raise ImproperlyConfigured("%(cls)s is missing the "
+                "redirect_field_name. Define %(cls)s.redirect_field_name or "
+                "override %(cls)s.get_redirect_field_name()." % {
+                "cls": self.__class__.__name__})
+
+        return self.redirect_field_name
+
+
+class LoginRequiredMixin(AccessMixin):
     """
     View mixin which verifies that the user is authenticated.
 
     NOTE:
         This should be the left-most mixin of a view.
     """
-    login_url = settings.LOGIN_URL  # LOGIN_URL from project settings
-    raise_exception = False  # Default whether to raise an exception to none
-    redirect_field_name = REDIRECT_FIELD_NAME  # Set by django.contrib.auth
-
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
             if self.raise_exception:
                 raise PermissionDenied  # return a forbidden response
             else:
                 return redirect_to_login(request.get_full_path(),
-                    self.login_url, self.redirect_field_name)
+                    self.get_login_url(), self.get_redirect_field_name())
 
         return super(LoginRequiredMixin, self).dispatch(request, *args,
             **kwargs)
