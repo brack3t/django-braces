606bd7f90621944441d57bd5232b80be50f35162
Chris Jones <chris@brack3t.com>
2014-03-01 20:57:58 -0800

    PEP8.

--- braces/views.py
+++ braces/views.py
@@ -8,7 +8,8 @@ from django.core import serializers
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.urlresolvers import resolve, reverse
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.http import (HttpResponse, HttpResponseBadRequest,
+                         HttpResponseRedirect)
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.utils.encoding import force_text
@@ -83,25 +84,26 @@ class AnonymousRequiredMixin(object):
     View mixin which redirects to a specified URL if authenticated.
     Can be useful if you wanted to prevent authenticated users from
     accessing signup pages etc.
-    
+
     NOTE:
         This should be the left-most mixin of a view.
-        
+
     Example Usage
-    
+
         class SomeView(AnonymousRequiredMixin, ListView):
             ...
             # required
             authenticated_redirect_url = "/accounts/profile/"
             ...
     """
-    authenticated_redirect_url = settings.LOGIN_REDIRECT_URL  # LOGIN_REDIRECT_URL from project settings
-    
+    authenticated_redirect_url = settings.LOGIN_REDIRECT_URL
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated():
             return HttpResponseRedirect(self.get_authenticated_redirect_url())
-        return super(AnonymousRequiredMixin, self).dispatch(request, *args, **kwargs)
-        
+        return super(AnonymousRequiredMixin, self).dispatch(
+            request, *args, **kwargs)
+
     def get_authenticated_redirect_url(self):
         # Return the reversed authenticated redirect url.
         if self.authenticated_redirect_url is None:
@@ -112,8 +114,7 @@ class AnonymousRequiredMixin(object):
                 "%(cls)s.get_authenticated_redirect_url()"
                 "." % {"cls": self.__class__.__name__})
         return reverse(self.authenticated_redirect_url)
-    
-    
+
 
 class CsrfExemptMixin(object):
     """
@@ -175,6 +176,7 @@ class PermissionRequiredMixin(AccessMixin):
 
         return super(PermissionRequiredMixin, self).dispatch(
             request, *args, **kwargs)
+
 
 class MultiplePermissionsRequiredMixin(AccessMixin):
     """
