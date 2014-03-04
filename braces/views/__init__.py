5adf1b9fc43d78414de7e27a2d5aed66af793941
Kenneth Love <kenneth@gigantuan.net>
2014-03-04 14:08:13 -0800

    ajax

--- braces/views/__init__.py
+++ braces/views/__init__.py
@@ -1,9 +1,23 @@
 from __future__ import absolute_import
 
+from ._ajax import (
+    JSONResponseMixin,
+    AjaxResponseMixin,
+    JsonRequestResponseMixin,
+    JSONRequestResponseMixin
+)
 from ._queries import (
     OrderableListMixin,
     SelectRelatedMixin,
     PrefetchRelatedMixin
 )
 
-__all__ = ['OrderableListMixin', 'SelectRelatedMixin', 'PrefetchRelatedMixin']
+__all__ = [
+    'OrderableListMixin',
+    'SelectRelatedMixin',
+    'PrefetchRelatedMixin',
+    'JSONResponseMixin',
+    'AjaxResponseMixin',
+    'JsonRequestResponseMixin',
+    'JSONRequestResponseMixin',
+]
