06acfe5bf40a21a990dbb2e2e946a6f22910cb48
Kenneth Love <kenneth@gigantuan.net>
2014-11-07 14:56:17 -0800

    got tests to run for at least one version of python and django

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import os
 from django.conf import settings
+from tests import settings as test_settings
 
 
 def pytest_configure():
-    if not settings.configured:
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
+    settings.configure(default_settings=test_settings)
