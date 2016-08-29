dd7185231769ab567c1d6ac056864fd1c29340c0
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-08-29 12:40:13 -0300

    fix compatibility of test suit with django 1.10

--- tests/compat.py
+++ tests/compat.py
@@ -9,6 +9,22 @@ except ImportError:
     from django.utils import simplejson as json
 
 try:
-    from django.conf.urls import patterns, url, include
+    from django.conf.urls import url, include
 except ImportError:
-    from django.conf.urls.defaults import patterns, url, include
+    from django.conf.urls.defaults import url, include
+
+
+def patterns_compat(urlpatterns):
+    try:
+        from django.conf.urls import patterns
+    except ImportError:
+        try:
+            from django.conf.urls.defaults import patterns
+        except ImportError:
+            patterns = False
+    if patterns:
+        return patterns(
+            '', *urlpatterns
+        )
+    else:
+        return urlpatterns
