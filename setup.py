84e36ed17823579aadbd6a0855cf9f8814fd9961
Kenneth Love <kennethlove@gmail.com>
2017-02-01 11:40:53 -0800

    version bump

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Framework :: Django",
         "Framework :: Django :: 1.5",
         "Framework :: Django :: 1.6",
