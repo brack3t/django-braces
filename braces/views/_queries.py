814be78c4dcb25ceebf9a8df54138ba42b2645ad
Hobbestigrou <natal@emencia.com>
2017-04-18 15:23:40 +0200

    [Core] The attribute can be empty so display a warning to alert the user.

--- braces/views/_queries.py
+++ braces/views/_queries.py
@@ -1,3 +1,5 @@
+import warnings
+
 from django.core.exceptions import ImproperlyConfigured
 
 
@@ -25,6 +27,10 @@ class SelectRelatedMixin(object):
         # Get the current queryset of the view
         queryset = super(SelectRelatedMixin, self).get_queryset()
 
+        if not self.select_related:
+            warnings.warn('The select_related attribute is empty')
+            return queryset
+
         return queryset.select_related(*self.select_related)
 
 
@@ -51,6 +57,10 @@ class PrefetchRelatedMixin(object):
 
         # Get the current queryset of the view
         queryset = super(PrefetchRelatedMixin, self).get_queryset()
+
+        if not self.prefetch_related:
+            warnings.warn('The prefetch_related attribute is empty')
+            return queryset
 
         return queryset.prefetch_related(*self.prefetch_related)
 
