71b04a4e07ea03e7bcc20630c0981bdbda12a9db
David Guillot <dguillot@contexte.com>
2017-07-06 15:58:04 +0200

    fix(ajax): fix #223 by allowing OPTIONS requests to be body-less

--- braces/views/_ajax.py
+++ braces/views/_ajax.py
@@ -133,7 +133,8 @@ class JsonRequestResponseMixin(JSONResponseMixin):
         self.kwargs = kwargs
 
         self.request_json = self.get_request_json()
-        if self.require_json and self.request_json is None:
+        if request.method != 'OPTIONS'\
+                and self.require_json and self.request_json is None:
             return self.render_bad_request_response()
         return super(JsonRequestResponseMixin, self).dispatch(
             request, *args, **kwargs)
