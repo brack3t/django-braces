5c7526ddfec0ca718e2a7379441efb5890609b5f
Kenneth Love <kennethlove@gmail.com>
2018-04-22 07:52:02 -0700

    Comma

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "Framework :: Django :: 1.7",
         "Framework :: Django :: 1.8",
         "Framework :: Django :: 1.9",
-        "Framework :: Django :: 1.10"
+        "Framework :: Django :: 1.10",
         "Framework :: Django :: 2.0"
     ],
 )
