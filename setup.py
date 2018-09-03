6dd211d7a24b392d9c9137b19e1480dd350f1c27
Michael Käufl <michael-k@users.noreply.github.com>
2018-09-03 21:33:50 +0200

    Run tests against Python 3.7

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Framework :: Django",
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.0",
