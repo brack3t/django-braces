78328dcbf716a9384d3549e28f8ff104bc5dc769
Kenneth Love <klove@oreilly.com>
2019-12-20 13:02:58 -0800

    prints

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,19 @@
-from django.conf.global_settings import *
+import os
 
 import dj_database_url
+
+from django.conf.global_settings import *
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    """Get the environment variable or raise exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the {} environemnt variable.".format(var_name)
+        raise ImproperlyConfigured(error_msg)
+
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -20,6 +33,11 @@ DATABASES['default'] = dj_database_url.config(
 )
 DATABASES['default']['TEST'] = {}
 DATABASES['default']['TEST']['NAME'] = DATABASES['default']['NAME']
+
+try:
+    print(get_env_variable("DATABASE_URL"))
+except ImproperlyConfigured as e:
+    print(e)
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
