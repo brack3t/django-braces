a2ba61e3bf6b77e9b61ba61cfe7bc0f6036857fa
Adam Johnson <me@adamj.eu>
2019-12-21 18:05:33 +0000

    Ditch PostgreSQL for testing

--- tests/settings.py
+++ tests/settings.py
@@ -1,95 +1,64 @@
-import os
-
-import dj_database_url
-
 from django.conf.global_settings import *
-from django.core.exceptions import ImproperlyConfigured
-
-
-def get_env_variable(var_name):
-    """Get the environment variable or raise exception."""
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        error_msg = "Set the {} environemnt variable.".format(var_name)
-        raise ImproperlyConfigured(error_msg)
-
 
 DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
-TIME_ZONE = 'UTC'
-LANGUAGE_CODE = 'en-US'
+TIME_ZONE = "UTC"
+LANGUAGE_CODE = "en-US"
 SITE_ID = 1
 USE_L10N = True
 USE_TZ = True
 
-SECRET_KEY = 'local'
+SECRET_KEY = "local"
 
-ROOT_URLCONF = 'tests.urls'
+ROOT_URLCONF = "tests.urls"
 
-try:
-    TRAVIS = get_env_variable("IN_TRAVIS")
-except ImproperlyConfigured:
-    TRAVIS = False
-
-print("="*50)
-print("TRAVIS: {}".format(TRAVIS))
-print("="*50)
-
-if TRAVIS:
-    DATABASES['default'] = dj_database_url.parse(get_env_variable("DATABASE_URL"))
-    DATABASES['default']['TEST'] = {}
-    DATABASES['default']['TEST']['NAME'] = DATABASES['default']['NAME']
-else:
-    DATABASES['default'] = dj_database_url.parse("sqlite:///:memory:")
+DATABASES = {
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"},
+}
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 MIDDLEWARE = MIDDLEWARE_CLASSES
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-
-    'tests',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "tests",
 )
 
-PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-)
+PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
