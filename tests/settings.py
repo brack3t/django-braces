bf28e976dcac8e4c4d86c6d263c0a33e71189243
Kenneth Love <klove@oreilly.com>
2019-12-23 10:01:39 -0800

    Have to use the right database engine

--- tests/settings.py
+++ tests/settings.py
@@ -15,7 +15,7 @@ ROOT_URLCONF = 'tests.urls'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:'
     }
 }
