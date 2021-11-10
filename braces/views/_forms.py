1f675ecb783449902848cc903cd56afb66d13b2b
Kenneth Love <klove@oreilly.com>
2021-11-10 13:15:47 -0800

    Modernization pass for the forms mixins.
    These are pretty good, no real notes on them.

--- braces/views/_forms.py
+++ braces/views/_forms.py
@@ -9,7 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
 
 
-class CsrfExemptMixin(object):
+class CsrfExemptMixin:
     """
     Exempts the view from CSRF requirements.
 
@@ -19,31 +19,32 @@ class CsrfExemptMixin(object):
 
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
-        return super(CsrfExemptMixin, self).dispatch(*args, **kwargs)
+        return super().dispatch(*args, **kwargs)
 
 
-class UserFormKwargsMixin(object):
+class UserFormKwargsMixin:
     """
-    CBV mixin which puts the user from the request into the form kwargs.
-    Note: Using this mixin requires you to pop the `user` kwarg
-    out of the dict in the super of your form's `__init__`.
+    Automatically include `request.user` in form kwargs.
+
+    ## Note
+    You will need to handle the `user` kwarg in your form. Usually
+    this means `user = kwargs.pop("user")` in your form's `__init__`.
     """
 
     def get_form_kwargs(self):
-        kwargs = super(UserFormKwargsMixin, self).get_form_kwargs()
+        kwargs = super().get_form_kwargs()
         # Update the existing form kwargs dict with the request's user.
         kwargs.update({"user": self.request.user})
         return kwargs
 
 
-class SuccessURLRedirectListMixin(object):
+class SuccessURLRedirectListMixin:
     """
-    Simple CBV mixin which sets the success url to the list view of
-    a given app. Set success_list_url as a class attribute of your
-    CBV and don't worry about overloading the get_success_url.
+    Automatically reverses `success_list_url` and returns that as
+    the `success_url` for a form view.
 
-    This is only to be used for redirecting to a list page. If you need
-    to reverse the url with kwargs, this is not the mixin to use.
+    This is meant to redirect to a view without arguments. If you need
+    to include arguments to `reverse`, you can omit this mixin.
     """
 
     success_list_url = None  # Default the success url to none
@@ -51,19 +52,20 @@ class SuccessURLRedirectListMixin(object):
     def get_success_url(self):
         # Return the reversed success url.
         if self.success_list_url is None:
+            class_name = self.__class__.__name__
             raise ImproperlyConfigured(
-                "{0} is missing a success_list_url "
-                "name to reverse and redirect to. Define "
-                "{0}.success_list_url or override "
-                "{0}.get_success_url().".format(self.__class__.__name__)
+                f"{class_name} is missing a success_list_url attribute. "
+                f"Define {class_name}.success_list_url or override "
+                f"{class_name}.get_success_url()."
             )
         return reverse(self.success_list_url)
 
 
-class _MessageAPIWrapper(object):
+class _MessageAPIWrapper:
     """
-    Wrap the django.contrib.messages.api module to automatically pass a given
-    request object as the first parameter of function calls.
+    Wrapper for the django.contrib.messages.api module.
+    Automatically pass a request object as the first parameter of
+    message function calls.
     """
 
     API = set(
@@ -86,59 +88,59 @@ class _MessageAPIWrapper(object):
             setattr(self, name, partial(api_fn, request))
 
 
-class _MessageDescriptor(object):
+class _MessageDescriptor:
     """
     A descriptor that binds the _MessageAPIWrapper to the view's
     request.
     """
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance, *args, **kwargs):
         return _MessageAPIWrapper(instance.request)
 
 
-class MessageMixin(object):
+class MessageMixin:
     """
     Add a `messages` attribute on the view instance that wraps
-    `django.contrib .messages`, automatically passing the current
+    `django.contrib.messages`, automatically passing the current
     request object.
     """
 
     messages = _MessageDescriptor()
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._class_name = self.__class__.__name__
+
 
 class FormValidMessageMixin(MessageMixin):
     """
-    Mixin allows you to set static message which is displayed by
-    Django's messages framework through a static property on the class
-    or programmatically by overloading the get_form_valid_message method.
+    Set a string to be sent via Django's messages framework when a form
+    passes validation.
     """
 
     form_valid_message = None  # Default to None
 
     def get_form_valid_message(self):
         """
-        Validate that form_valid_message is set and is either a
-        unicode or str object.
+        Validate that form_valid_message is set correctly
         """
         if self.form_valid_message is None:
             raise ImproperlyConfigured(
-                "{0}.form_valid_message is not set. Define "
-                "{0}.form_valid_message, or override "
-                "{0}.get_form_valid_message().".format(self.__class__.__name__)
+                f"{self._class_name}.form_valid_message is not set. Define "
+                f"{self._class_name}.form_valid_message, or override "
+                f"{self._class_name}.get_form_valid_message()."
             )
 
         if not isinstance(self.form_valid_message, (str, Promise)):
             raise ImproperlyConfigured(
-                "{0}.form_valid_message must be a str or unicode "
-                "object.".format(self.__class__.__name__)
+                f"{self._class_name}.form_valid_message must be a str or Promise."
             )
 
         return force_str(self.form_valid_message)
 
     def form_valid(self, form):
         """
-        Call the super first, so that when overriding
-        get_form_valid_message, we have access to the newly saved object.
+        Set the "form valid" message for standard form validation
         """
         response = super(FormValidMessageMixin, self).form_valid(form)
         self.messages.success(
@@ -147,6 +149,9 @@ class FormValidMessageMixin(MessageMixin):
         return response
 
     def delete(self, *args, **kwargs):
+        """
+        Set the "form valid" message for delete form validation
+        """
         response = super(FormValidMessageMixin, self).delete(*args, **kwargs)
         self.messages.success(
             self.get_form_valid_message(), fail_silently=True
@@ -156,36 +161,34 @@ class FormValidMessageMixin(MessageMixin):
 
 class FormInvalidMessageMixin(MessageMixin):
     """
-    Mixin allows you to set static message which is displayed by
-    Django's messages framework through a static property on the class
-    or programmatically by overloading the get_form_invalid_message method.
+    Set a string to be sent via Django's messages framework when a form
+    fails validation.
     """
 
     form_invalid_message = None
 
     def get_form_invalid_message(self):
         """
-        Validate that form_invalid_message is set and is either a
-        unicode or str object.
+        Validate that form_invalid_message is set correctly.
         """
         if self.form_invalid_message is None:
             raise ImproperlyConfigured(
-                "{0}.form_invalid_message is not set. Define "
-                "{0}.form_invalid_message, or override "
-                "{0}.get_form_invalid_message().".format(
-                    self.__class__.__name__
-                )
+                f"{self._class_name}.form_invalid_message is not set. Define "
+                f"{self._class_name}.form_invalid_message, or override "
+                f"{self._class_name}.get_form_invalid_message()."
             )
 
         if not isinstance(self.form_invalid_message, (str, Promise)):
             raise ImproperlyConfigured(
-                "{0}.form_invalid_message must be a str or unicode "
-                "object.".format(self.__class__.__name__)
+                f"{self._class_name}.form_invalid_message must be a str or Promise."
             )
 
         return force_str(self.form_invalid_message)
 
     def form_invalid(self, form):
+        """
+        Set the "form invalid" message for standard form validation
+        """
         response = super(FormInvalidMessageMixin, self).form_invalid(form)
         self.messages.error(
             self.get_form_invalid_message(), fail_silently=True
@@ -195,8 +198,5 @@ class FormInvalidMessageMixin(MessageMixin):
 
 class FormMessagesMixin(FormValidMessageMixin, FormInvalidMessageMixin):
     """
-    Mixin is a shortcut to use both FormValidMessageMixin and
-    FormInvalidMessageMixin.
+    Set messages to be sent whether a form is valid or invalid.
     """
-
-    pass
