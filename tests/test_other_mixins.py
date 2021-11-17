82c65d66ac3e64ae898e559a0e600780a098c262
Kenneth Love <klove@oreilly.com>
2021-11-17 12:39:09 -0800

    An alternate way to handle custom headers
    This uses a new TemplateResponse which will let us more cleanly inject headers into the response.

--- tests/test_other_mixins.py
+++ tests/test_other_mixins.py
@@ -824,6 +824,10 @@ class TestHeaderMixin(test.TestCase):
         response = self.client.get('/headers/existing/')
         self.assertEqual(response['X-DJANGO-BRACES-EXISTING'], 'value')
 
+    def test_template(self):
+        response = self.client.get('/headers/template/')
+        self.assertEqual(response['X-DJANGO-BRACES-1'], '1')
+
 class TestCacheControlMixin(test.TestCase):
     def test_cachecontrol_public(self):
         response = self.client.get('/cachecontrol/public/')
