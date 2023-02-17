7a33115b950ccaf896b6d4de8981734b8105be31
Kenneth Love <klove@oreilly.com>
2023-02-17 15:49:58 -0800

    Messages mixins and tests

---
+++ braces/mixins/_messages.py
@@ -0,0 +1,127 @@
+from functools import partial
+
+from django.contrib import messages
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.functional import Promise
+
+
+
+class _MessageHelper:
+    API = set([
+        "add_message",
+        "get_messages",
+        "get_level",
+        "set_level",
+        "debug",
+        "info",
+        "success",
+        "warning",
+        "error",
+        "MessageFailure",
+    ])
+
+    def __init__(self, request):
+        for name in self.API:
+            api_fn = getattr(messages.api, name)
+            setattr(self, name, partial(api_fn, request))
+
+
+class _MessageDescriptor:
+    """
+    A descriptor that wraps `django.contrib.messages` and automatically
+    passes the current request object.
+    """
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return _MessageHelper(instance.request)
+
+    def __set__(self, instance, value):
+        raise AttributeError("Cannot set the 'messages' attribute.")
+
+    def __delete__(self, instance):
+        raise AttributeError("Cannot delete the 'messages' attribute.")
+
+
+class MessagesMixin:
+    messages = _MessageDescriptor()
+
+
+class FormValidMessageMixin(MessagesMixin):
+    form_valid_message: str = None
+
+    def get_form_valid_message(self):
+        name = self.__class__.__name__
+        if not getattr(self, "form_valid_message", None):
+            raise ImproperlyConfigured(
+                f"{name}.form_valid_message is not set. "
+                f"Define {name}.form_valid_message, or "
+                f"override {name}.get_form_valid_message()."
+            )
+        if self.form_valid_message is None:
+            raise ImproperlyConfigured(
+                f"{name}.form_valid_message is not set. "
+                f"Define {name}.form_valid_message, or "
+                f"override {name}.get_form_valid_message()."
+            )
+
+        if not isinstance(self.form_valid_message, (str, Promise)):
+            raise ImproperlyConfigured(
+                f"{name}.form_valid_message must be a str."
+            )
+
+        return self.form_valid_message
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self.messages.success(
+            self.get_form_valid_message(), fail_silently=True
+        )
+        return response
+
+    def delete(self, *args, **kwargs):
+        response = super().delete(*args, **kwargs)
+        self.messages.success(
+            self.get_form_valid_message(), fail_silently=True
+        )
+        return response
+
+
+class FormInvalidMessageMixin(MessagesMixin):
+    form_invalid_message: str = None
+
+    def get_form_invalid_message(self):
+        name = self.__class__.__name__
+
+        if not getattr(self, "form_invalid_message", None):
+            raise ImproperlyConfigured(
+                f"{name}.form_invalid_message is not set. "
+                f"Define {name}.form_invalid_message, or "
+                f"override {name}.get_form_invalid_message()."
+            )
+
+        if self.form_invalid_message is None:
+            raise ImproperlyConfigured(
+                f"{name}.form_invalid_message is not set. "
+                f"Define {name}.form_invalid_message, or "
+                f"override {name}.get_form_invalid_message()."
+            )
+
+        if not isinstance(self.form_invalid_message, (str, Promise)):
+            raise ImproperlyConfigured(
+                f"{name}.form_invalid_message must be a str."
+            )
+
+        return self.form_invalid_message
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        self.messages.error(
+            self.get_form_invalid_message(), fail_silently=True
+        )
+        return response
+
+
+class FormMessagesMixin(FormValidMessageMixin, FormInvalidMessageMixin):
+    pass
---
+++ tests/test_messages.py
@@ -0,0 +1,136 @@
+import pytest
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.messages.middleware import MessageMiddleware
+from django.core.exceptions import ImproperlyConfigured
+from django.views.generic import View
+from django.test import RequestFactory
+
+from braces import mixins
+
+
+class TestMessages:
+    middleware = MessageMiddleware("")
+
+    class _View(mixins.MessagesMixin, View):
+        def get(self, request):
+            self.messages.add_message(messages.INFO, "test")
+            return HttpResponse("OK")
+
+    def get_request(self):
+        request = RequestFactory().get("/")
+        self.middleware.process_request(request)
+        return request
+
+    def get_response(self, request, view=_View):
+        _view = view.as_view()
+        response = _view(request)
+        self.middleware.process_response(request, response)
+        return response
+
+    def get_request_response(self, view):
+        request = self.get_request()
+        response = self.get_response(request, view)
+        return request, response
+
+    @pytest.mark.parametrize("level", [
+        messages.DEBUG, messages.INFO, messages.SUCCESS,
+        messages.WARNING, messages.ERROR])
+    def test_add_messages(self, level):
+        class _View(self._View):
+            def get(self, request):
+                self.messages.add_message(level, "test")
+                return HttpResponse("OK")
+
+        req, _ = self.get_request_response(_View)
+        _messages = list(req._messages)
+        assert len(_messages) == 1
+        assert _messages[0].message == "test"
+        assert _messages[0].level == level
+
+
+class TestFormValidMessage:
+    middleware = MessageMiddleware("")
+
+    class _View(mixins.FormValidMessageMixin, View):
+        form_valid_message = "valid"
+
+        def get(self, request):
+            return HttpResponse("OK")
+
+    def test_form_valid_message(self):
+        assert self._View().get_form_valid_message() == "valid"
+
+    def test_form_valid_message_none(self):
+        class _View(self._View):
+            form_valid_message = None
+
+        with pytest.raises(ImproperlyConfigured):
+            _View().get_form_valid_message()
+
+    def test_form_valid_message_not_str(self):
+        class _View(self._View):
+            form_valid_message = 1
+
+        with pytest.raises(ImproperlyConfigured):
+            _View().get_form_valid_message()
+
+    def test_form_valid_message_not_set(self):
+        class _View(mixins.FormValidMessageMixin, View):
+            def get(self, request):
+                return HttpResponse("OK")
+
+        with pytest.raises(ImproperlyConfigured):
+            _View().get_form_valid_message()
+
+
+class TestFormInvalidMessage:
+    middleware = MessageMiddleware("")
+
+    class _View(mixins.FormInvalidMessageMixin, View):
+        form_invalid_message = "invalid"
+
+        def get(self, request):
+            return HttpResponse("OK")
+
+    def get_request(self):
+        request = RequestFactory().get("/")
+        self.middleware.process_request(request)
+        return request
+
+    def get_response(self, request, view=_View):
+        _view = view.as_view()
+        response = _view(request)
+        self.middleware.process_response(request, response)
+        return response
+
+    def get_request_response(self, view):
+        request = self.get_request()
+        response = self.get_response(request, view)
+        return request, response
+
+    def test_form_invalid_message(self):
+        _view = self._View()
+        assert _view.get_form_invalid_message() == "invalid"
+
+    def test_form_invalid_message_none(self):
+        class _View(self._View):
+            form_invalid_message = None
+
+        with pytest.raises(ImproperlyConfigured):
+            _View().get_form_invalid_message()
+
+    def test_form_invalid_message_not_str(self):
+        class _View(self._View):
+            form_invalid_message = 1
+
+        with pytest.raises(ImproperlyConfigured):
+            _View().get_form_invalid_message()
+
+    def test_form_valid_message_not_set(self):
+        class _View(mixins.FormInvalidMessageMixin, View):
+            def get(self, request):
+                return HttpResponse("OK")
+
+        with pytest.raises(ImproperlyConfigured):
+            _View().get_form_invalid_message()
