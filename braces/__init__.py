f6f0f9bb6ccac845f7c7a17ec6df8cfb47e741b8
Kenneth Love <kennethlove@gmail.com>
2023-02-20 22:00:25 -0800

    Updated docstring and version

--- braces/__init__.py
+++ braces/__init__.py
@@ -1,16 +1,12 @@
 """
-django-braces
--------------
+django-braces: generic view mixins
 
-Mixins and view classes to make working with Django's generic
-class-based views simpler.
-
-:copyright: (c) 2013 by Kenneth Love and Chris Jones
-:license: BSD 3-clause. See LICENSE for more details
+Mixins and view classes to make working with Django's
+generic class-based views simpler.
 """
 
 __title__ = "braces"
-__version__ = "2.0"
+__version__ = "2.0.0"
 __author__ = "Kenneth Love and Chris Jones"
 __license__ = "BSD 3-clause"
 __copyright__ = "Copyright 2013 Kenneth Love and Chris Jones"
