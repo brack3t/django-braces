e9b44b1ebdc65a46391b2b450255e82b32d60aed
Kenneth Love <klove@oreilly.com>
2023-02-22 16:11:04 -0800

    MultipleForm and MultipleModelForm mixins and tests

--- braces/mixins/_forms.py
+++ braces/mixins/_forms.py
@@ -1,5 +1,8 @@
+from typing import List, Dict
+
 from django import forms
 from django.core.exceptions import ImproperlyConfigured
+from django.db import models
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
@@ -66,3 +69,102 @@ class SuccessURLRedirectMixin(mixins.RedirectMixin):
 
     def get_redirect_url(self) -> str:
         return self.get_success_url()
+
+
+class MultipleFormsMixin:
+    form_classes: Dict[str, forms.Form] = None
+    initial: Dict[str, Dict] = {}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.get_form = self.get_forms
+
+    def get_context_data(self, **kwargs) -> Dict:
+        context = super().get_context_data(**kwargs)
+        context["forms"] = self.get_forms()
+        return context
+
+    def get_form_classes(self) -> List:
+        if self.form_classes is None:
+            name = self.__class__.__name__
+            raise ImproperlyConfigured(
+                f"{name} is missing a form_classes. Define "
+                f"{name}.form_classes, or override "
+                f"{name}.get_form_classes()."
+            )
+
+        if not isinstance(self.form_classes, dict):
+            raise ImproperlyConfigured(
+                f"{name}.form_classes must be a dict."
+            )
+
+        return self.form_classes
+
+    def get_forms(self) -> Dict[str, forms.Form]:
+        forms = {}
+        for name, form_class in self.get_form_classes().items():
+            forms[name] = form_class(**self.get_form_kwargs(name))
+        return forms
+
+    def get_form_kwargs(self, name) -> Dict:
+        kwargs = {
+            "prefix": name,
+        }
+
+        initial = self.get_initial()
+        if name in initial:
+            kwargs["initial"] = initial[name]
+
+        if self.request.method in ("POST", "PUT", "PATCH"):
+            kwargs["data"] = self.request.POST
+            kwargs["files"] = self.request.FILES
+
+        return kwargs
+
+    def validate_forms(self) -> bool:
+        forms = self.get_forms()
+        return all(form.is_valid() for form in forms.values())
+
+    def forms_valid(self) -> None:
+        raise NotImplementedError
+
+    def forms_invalid(self) -> None:
+        raise NotImplementedError
+
+    def post(self, request, *args, **kwargs):
+        if self.validate_forms():
+            return self.forms_valid()
+        return self.forms_invalid()
+
+    def put(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
+class MultipleModelFormsMixin(MultipleFormsMixin):
+    instances: Dict[str, models.Model] = None
+
+    def get_instances(self) -> Dict[str, models.Model]:
+        if self.instances is None:
+            name = self.__class__.__name__
+            raise ImproperlyConfigured(
+                f"{name} is missing a instances. Define "
+                f"{name}.instances, or override "
+                f"{name}.get_instances()."
+            )
+
+        if not isinstance(self.instances, dict):
+            raise ImproperlyConfigured(
+                f"{name}.instances must be a dict."
+            )
+
+        return self.instances
+
+    def get_form_kwargs(self, name) -> Dict:
+        kwargs = super().get_form_kwargs(name)
+        instances = self.get_instances()
+        if name in instances:
+            kwargs["instance"] = instances[name]
+        return kwargs
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,8 +1,10 @@
 import pytest
 from django import forms
+from django.core.exceptions import ImproperlyConfigured
 from django.views.generic import FormView
 
 from braces import mixins
+from .project.models import Article, CanonicalArticle
 
 
 
@@ -81,3 +83,91 @@ class TestSuccessURLRedirect:
         view.setup(request)
         assert view.get_success_url() == "/"
         assert view.get_redirect_url() == "/"
+
+
+class TestMultipleFormsMixin:
+    def setup_method(self):
+        self.Form1 = type("Form1", (forms.Form,), {"name": forms.CharField()})
+        self.Form2 = type("Form2", (forms.Form,), {"age": forms.IntegerField()})
+
+        class _View(mixins.MultipleFormsMixin, FormView):
+            form_classes = {
+                "my_form": self.Form1,
+                "my_form2": self.Form2
+            }
+            template_name = "test.html"
+
+        self._View = _View
+
+    def test_missing_attribute(self):
+        class InvalidForm(mixins.MultipleFormsMixin, FormView):
+            pass
+
+        with pytest.raises(ImproperlyConfigured):
+            InvalidForm().get_form_classes()
+
+    def test_form_classes(self):
+        assert self._View().get_form_classes() == {"my_form": self.Form1, "my_form2": self.Form2}
+
+    def test_forms_in_context(self, rf):
+        request = rf.get("/")
+        view = self._View()
+        view.setup(request)
+        view.get(request)
+        assert "my_form" in view.get_context_data()["forms"]
+        assert "my_form2" in view.get_context_data()["forms"]
+
+    def test_forms_with_initial_values(self, rf):
+        request = rf.get("/")
+        view = self._View()
+        view.initial = {"my_form": {"name": "bar"}}
+        view.setup(request)
+        view.get(request)
+        assert view.get_context_data()["forms"]["my_form"].initial == {"name": "bar"}
+        assert view.get_context_data()["forms"]["my_form2"].initial == {}
+
+    def test_forms_valid(self, rf):
+        request = rf.post("/", data={"my_form-name": "foo", "my_form2-age": 42})
+        view = self._View()
+        view.setup(request)
+        assert view.get_context_data()["forms"]["my_form"].is_valid()
+        assert view.get_context_data()["forms"]["my_form2"].is_valid()
+        assert view.validate_forms()
+
+    def test_not_implemented(self):
+        view = self._View()
+        with pytest.raises(NotImplementedError):
+            view.forms_valid()
+
+        with pytest.raises(NotImplementedError):
+            view.forms_invalid()
+
+
+class TestMultipleModelFormsMixin:
+    class _Form1(forms.ModelForm):
+        class Meta:
+            model = Article
+            fields = ["title"]
+
+    class _Form2(forms.ModelForm):
+        class Meta:
+            model = CanonicalArticle
+            fields = ["title"]
+
+    def setup_method(self):
+        article = Article.objects.create(title="foo")
+        class _View(mixins.MultipleModelFormsMixin, FormView):
+            form_classes = {
+                "my_form": self._Form1,
+                "my_form2": self._Form2
+            }
+            instances = {"my_form": article}
+            template_name = "test.html"
+
+        self._View = _View
+
+    @pytest.mark.django_db
+    def test_get_instances(self):
+        assert self._View().get_instances() == {
+            "my_form": Article.objects.get(title="foo")
+        }
